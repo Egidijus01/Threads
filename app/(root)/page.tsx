e46b6ae9399547aa54d948d5b8c9b0873e97dd8a
Egidijus <egidijusgagela@gmail.com>
2023-10-19 18:47:52 +0300

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import { UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function Home() {
+
+  return (
+   
+    <>
+      <h1 className="head-text text-left">Home</h1>
+      
+    </>  
+      
+  );
+}
+
+
+
+
+
+
+// import { UserButton } from "@clerk/nextjs";
+ 
+// export default function Home() {
+//   return (
+//     <div>
+//       <UserButton afterSignOutUrl="/"/>
+//     </div>
+//   )
+// }
